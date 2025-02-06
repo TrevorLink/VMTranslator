6e92c1a6e547ca1de81d32813d21d4336cbe00c0
trevorlink <2845964844@qq.com>
2025-02-06 17:08:10 +0800

    AsmCodeWriter save state

--- src/main/java/com/noctis/vm/translator/common/VMConstants.java
+++ src/main/java/com/noctis/vm/translator/common/VMConstants.java
@@ -1,8 +1,6 @@
 package com.noctis.vm.translator.common;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Noctis
@@ -12,13 +10,22 @@ public class VMConstants {
 
    public static final String INSTRUCTION_PATTERN = "^(push|pop)\\s(constant|local|argument|this|that|static|temp|pointer)\\s\\d+$";
 
-   public static Set<String> ARITHMETIC_COMMAND_SET = new HashSet<>(Arrays.asList("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"));
+   public static final Set<String> ARITHMETIC_COMMAND_SET = new HashSet<>(Arrays.asList("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"));
 
-   public static String PUSH_COMMAND_PREFIX = "push";
+   public static final String PUSH_COMMAND_PREFIX = "push";
 
-   public static String POP_COMMAND_PREFIX = "pop";
+   public static final String POP_COMMAND_PREFIX = "pop";
 
-   public static String COMMENT_IDENTIFIER = "//";
+   public static final String COMMENT_IDENTIFIER = "//";
 
-   public static String RESULT_ASM_FILE_SUFFIX = ".asm";
+   public static final String RESULT_ASM_FILE_SUFFIX = ".asm";
+
+   public static final Map<String, String> virtualSegmentAndIdentifierMap = new HashMap<>(4);
+
+   static {
+      virtualSegmentAndIdentifierMap.put("local", "@LCL");
+      virtualSegmentAndIdentifierMap.put("argument", "@ARG");
+      virtualSegmentAndIdentifierMap.put("this", "@THIS");
+      virtualSegmentAndIdentifierMap.put("that", "@THAT");
+   }
 }
--- src/main/java/com/noctis/vm/translator/writer/AssemblyCodeWriter.java
+++ src/main/java/com/noctis/vm/translator/writer/AssemblyCodeWriter.java
@@ -14,11 +14,13 @@ import java.io.IOException;
 public class AssemblyCodeWriter {
 
    private final FileWriter fw;
+   private final String fileName;
 
    public AssemblyCodeWriter(String fileName) {
       if (StringUtils.isEmpty(fileName)) {
          throw new RuntimeException("Empty result file name");
       }
+      this.fileName = fileName;
       fileName = fileName + VMConstants.RESULT_ASM_FILE_SUFFIX;
       try {
          fw = new FileWriter(fileName);
@@ -38,10 +40,10 @@ public class AssemblyCodeWriter {
       String content;
       switch (instructionType) {
          case C_PUSH:
-            content = translatePushCommandToAssembly(segment, index);
+            content = translatePushCommandToAssembly(segment, index, fileName);
             break;
          case C_POP:
-            content = translatePopCommandToAssembly(segment, index);
+            content = translatePopCommandToAssembly(segment, index, fileName);
             break;
          case C_ARITHMETIC:
          default:
@@ -58,11 +60,22 @@ public class AssemblyCodeWriter {
       }
    }
 
-   private String translatePushCommandToAssembly(String segment, Integer index) {
+   private String translatePushCommandToAssembly(String segment, Integer index, String fileName) {
+      //constant
+      String constant = "@i\nD=A\n@SP\nA=M\nM=D\n";
+      //local,argument,this,that
+      String localAndSoOn = "D=M\n@i\nD=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n";
+      //static
+      String statics = "@" + fileName + "." + index + "\n";
+      //temp
+      String tmp = "@5\nD=A\n@i\nD=D+A\n@SP\nA=M\nM=D\n";
+      //pointer push pointer 0
+      String ptr = "@3\nD=A\n@i\nA=D+A\nA=M\nD=M\n@SP\nA=M\nM=D\n";
+      String spIncrement = "@SP\nM=M+1\n";
       return "";
    }
 
-   private String translatePopCommandToAssembly(String segment, Integer index) {
+   private String translatePopCommandToAssembly(String segment, Integer index, String fileName) {
       return "";
    }
 
